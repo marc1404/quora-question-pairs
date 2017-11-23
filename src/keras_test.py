f831ca5beaea953c40fb04cf9bf29aeebc755554
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-24 00:46:27 +0100

    Update .gitignore and add keras test

---
+++ src/keras_test.py
@@ -0,0 +1,15 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+
+model.add(Dense(units=64, activation='relu', input_dim=100))
+model.add(Dense(units=10, activation='softmax'))
+
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer='sgd',
+    metrics=['accuracy']
+)
+
+print('Everything seems to be fine :)')
