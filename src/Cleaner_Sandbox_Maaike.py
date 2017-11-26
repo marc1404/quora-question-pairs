5c60ef2c0fa92390b01784beeffdde728b9074fc
Maaike Koolbergen <maaikekoolbergen@gmail.com>
2017-11-26 20:14:57 +0100

    added the clean the text lines (to be expanded maybe)

--- src/Cleaner_Sandbox_Maaike.py
+++ src/Cleaner_Sandbox_Maaike.py
@@ -2,6 +2,7 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from gensim.models import KeyedVectors
+import re
 
 # load training and test data sets
 train_df = pd.read_csv(r"C:\Users\Maaik\quora-question-pairs\data\train_data.csv")
@@ -14,8 +15,40 @@ stops = set(stopwords.words('english'))
 def text_to_word_list(text):
     # pre process and convert text into a list of words
     text = str(text)
-    text = text.lower()
-    text = text.split()
+    text = text.lower
+    # Let's start scrubbing the text ;D and do a little bit of stemming
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text) # removes 's from words
+    text = re.sub(r"\'ve", " have ", text) # changes the ending of words 've into: <blank space> have
+    text = re.sub (r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text) # remove commas
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"''", " ", text)
+    # text = re.sub(r"(\d+)(k)", r"\g<1>000", text) < dont know what it does (yet)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " america ", text)
+    text = re.sub(r" usa ", " america ", text)
+    text = re.sub(r" United States ", " america ", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r" uk ", " england ", text)
+    text = re.sub(r" the us ", " america ", text)
+
+
+    text = text.split() # split into words by white space
     return text
 
 # this is going to be our vocabulary, we will give it a word and it will give us the index
@@ -29,12 +62,8 @@ inverse_vocabulary = ['<unk>']
 
 #embedding
 word2vec = KeyedVectors.load_word2vec_format(embedding_file, binary=True)
-
 # %%
 print('Done loading word2vec')
-
-
-
 # iterate over the questions only of both training and test dataset
 for dataset in [train_df, test_df]:
     for index, row in dataset.iterrows():
@@ -55,7 +84,7 @@ for dataset in [train_df, test_df]:
 
                 question_as_vector.append(word_to_index[word])
 
-            # replaces questions with lists of word indices:
+            # replaces questions with lists of word indices = number representation:
             dataset.set_value(index, question_column, question_as_vector)
 
 
