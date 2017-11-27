2c23eff8a1e32776f0daf0e35ea86059d1ab5a2b
Maaike Koolbergen <maaikekoolbergen@gmail.com>
2017-11-27 12:02:47 +0100

    made changes, got stuff to work =D

--- src/playing_around_maaike.py
+++ src/playing_around_maaike.py
@@ -2,5 +2,15 @@ import pandas as pd
 
 train_df = pd.read_csv(r"C:\Users\Maaik\quora-question-pairs\data\train_data.csv")
 test_df = pd.read_csv(r"C:\Users\Maaik\quora-question-pairs\data\train_data.csv")
+trainmerge_df = pd.read_csv(r"C:\Users\Maaik\quora-question-pairs\data\train_data_merge.csv")
 
+#%%
 print('Total number of question pairs for training: {}'.format(len(train_df)))
+
+#%%
+train_df.head()
+
+#%%
+print('Duplicate pairs: {}%'.format(round(trainmerge_df['is_duplicate'].mean()*100, 2)))
+
+#%%
