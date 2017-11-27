ccc3e044838d512f2455a1719a5f1e6e0e0cfa67
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-27 15:25:01 +0100

    Add sandbox and util folder

---
+++ src/sandbox/longest_question.py
@@ -0,0 +1,14 @@
+import src.csv as csv
+
+columns = ['question1', 'question2']
+train_df = csv.parse('data/train_vector.csv', columns)
+test_df = csv.parse('data/test_vector.csv', columns)
+
+longest = max(
+    train_df.question1.map(lambda x: len(x)).max(),
+    train_df.question2.map(lambda x: len(x)).max(),
+    test_df.question1.map(lambda x: len(x)).max(),
+    test_df.question2.map(lambda x: len(x)).max()
+)
+
+print(longest)
